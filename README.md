04335191a4f0686fc8f035647a87bd46d0614ea6
jeffrey forman <jeff@jeffreyforman.net>
2018-03-30 06:36:10 -0400

    initial version of a working bot! echoes a standard response, with documentation

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# Hangbot Readme
+# Hangbot
+
+Hangbot is a bot in the style of many other IRC/Slack bots, but for use on Google's new [Hangouts Chat](https://gsuite.google.com/products/chat/) product.
+
+## Features
+
+*  Receive messages via Pub/Sub topic subscription.
+*  Asynchronous responses via HTTP API.
+
+## Requirements
+
+From the [Hangouts Chat](https://developers.google.com/hangouts/chat/) documentation.
+
+*  ServiceAccount for the bot. To be used for receiving messages via Pub/Sub.
+*  Pub/Sub topic created to be used for your Bot to receive messages.
+
+## Screenshot of typical interaction
+
+![Screenshot](https://github.com/jforman/homebot/blob/master/hangouts-chat-screenshot.png)
+
+## Execution
+
+```bash
+$ ./hangbot.go -credentialsFile account.json \
+-project fooProject \
+-topic hangouts-chat.homebot \
+-subscription HomeSub
+```
+
+## Reference Material
+
+*  https://godoc.org/google.golang.org/api/chat/v1
+*  https://github.com/google/google-api-go-client/tree/master/chat/v1
+*  Basically everything under https://developers.google.com/hangouts/chat/concepts
+*  Pub/Sub client library quick start: https://developers.google.com/hangouts/chat/how-tos/bots-publish?authuser=0
+*  https://github.com/google/google-api-go-client/blob/master/GettingStarted.md
+
+## Future Development Ideas
+
+* Use the [cards](https://developers.google.com/hangouts/chat/concepts/cards) concept to make more rich interactions with the Bot.
+* Interactions with [Nestmon](https://github.com/jforman/nestmon), some Go code I wrote to stream Nest thermostat updates from my house. First just a way to see the current cooling/heating status of my home. Eventually maybe some sort of write capabilities to modify temperatures inside my home.
--- hangbot.go
+++ hangbot.go
@@ -1,59 +1,87 @@
-// Google Hangouts Chat Bot providing updates from Nest thermostats.
-
 package main
 
 import (
-  "flag"
-  "fmt"
-  "log"
+	"encoding/json"
+	"flag"
+	"log"
+	"os"
 
-  "cloud.google.com/go/pubsub"
-  "golang.org/x/net/context"
-  "google.golang.org/api/option"
-
+	"cloud.google.com/go/pubsub"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/chat/v1"
+	"google.golang.org/api/option"
 )
 
 var (
-  credentialsFile = flag.String("credentialsFile", "", "path to credentials file")
-  project = flag.String("project", "", "Google Cloud Project name")
-  psTopic = flag.String("topic", "", "Pub/Sub Topic for Hangouts Chat")
-  psSubscription = flag.String("subscription", "", "Pub/Sub Subscription for Hangouts Chat")
+	credentialsFile = flag.String("credentialsFile", "", "path to credentials file")
+	project         = flag.String("project", "", "Google Cloud Project name")
+	psTopic         = flag.String("topic", "", "Pub/Sub Topic for Hangouts Chat")
+	psSubscription  = flag.String("subscription", "", "Pub/Sub Subscription for Hangouts Chat")
+	incomingMessage *chat.Message
+	responseMessage *chat.Message
 )
 
 func main() {
-  flag.Parse()
-  fmt.Println("Hangbot Starting.")
-  fmt.Printf("Configuration: Credentials File: %s, Project: %s, Topic: %s, Subscription: %s.\n", *credentialsFile, *project, *psTopic, *psSubscription)
-  ctx := context.Background()
+	flag.Parse()
+	log.Println("Hangbot Starting.")
+	log.Printf("Configuration: Credentials File: %s, Project: %s, Topic: %s, Subscription: %s.\n", *credentialsFile, *project, *psTopic, *psSubscription)
 
-  client, err := pubsub.NewClient(ctx, *project, option.WithCredentialsFile(*credentialsFile))
+	// This seems like a hack, but some of the oauth libraries expect an environment variable
+	// if you use the JSON file, as opposed to being able to specify the path
+	// as part of client creation.
+	os.Setenv("GOOGLE_APPLICATION_CREDENTIALS", *credentialsFile)
+	ctx := context.Background()
 
-  if err != nil {
-    log.Fatalln("error creating newclient: %v.\n", err)
-  }
+	client, err := pubsub.NewClient(ctx, *project, option.WithCredentialsFile(*credentialsFile))
 
-  sub := client.Subscription(*psSubscription)
+	if err != nil {
+		log.Fatalln("error creating newclient: %v.\n", err)
+	}
 
-  cctx, _ := context.WithCancel(ctx)
+	sub := client.Subscription(*psSubscription)
 
-  ok, err := sub.Exists(ctx)
-  if err != nil {
-    log.Fatalf("Error checking if subscription exists. Err: %v.", err)
-  }
-  if ok {
-    log.Println("Subscription exists. Let's go.")
-  } else {
-    log.Fatalln("Checked if subscription exists. It doesn't.")
-  }
+	httpClient, err := google.DefaultClient(oauth2.NoContext, "https://www.googleapis.com/auth/chat.bot")
+	if err != nil {
+		log.Fatalf("Error creating httpClient: %v.\n", err)
+	}
 
-  err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
-    fmt.Printf("Message: Data: %s, More: %+v.\n", string(msg.Data), msg)
-    msg.Ack()
-  })
+	chatService, err := chat.New(httpClient)
+	if err != nil {
+		log.Fatalf("Error creating chatService: %v.\n", err)
+	}
 
-  // if err != context.Canceled {
-  //   return err
-  // }
+	sms := chat.NewSpacesMessagesService(chatService)
 
-  fmt.Println("Hangbot Exiting.")
+	cctx, _ := context.WithCancel(ctx)
+
+	ok, err := sub.Exists(ctx)
+	if err != nil {
+		log.Fatalf("Error checking if subscription exists. Err: %v.", err)
+	}
+	if !ok {
+		log.Fatalln("Checked if subscription exists. It doesn't.")
+	}
+
+	err = sub.Receive(cctx, func(ctx context.Context, msg *pubsub.Message) {
+		log.Printf("Received Message %s.\n", string(msg.Data))
+		msg.Ack()
+
+		err := json.Unmarshal(msg.Data, &incomingMessage)
+		if err != nil {
+			log.Fatalf("Unable to decode Chat Message JSON: %v.\n", err)
+		}
+
+		responseMessage = new(chat.Message)
+		responseMessage.Text = "Generic response."
+
+		response, err := sms.Create(incomingMessage.Space.Name, responseMessage).Do()
+		if err != nil {
+			log.Printf("There was an error sending a response back to Hangouts Chat: %v.\n", err)
+		}
+		log.Printf("Hangouts Response: %+v.\n", response)
+	})
+
+	log.Println("Hangbot Exiting.")
 }
